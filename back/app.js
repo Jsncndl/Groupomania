7dd3c177373b8450c014c3def8cde18d63fde0a3
Jsncndl <jsncndl1@gmail.com>
2022-09-05 15:04:38 +0200

    first commit: create nodeJS server, create express router, initialize dependencies

---
+++ back/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
+const app = express()
+
+const userRoutes = require('./routes/user')
+
+mongoose.connect(process.env.DB_AUTH, { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => console.log("Connect to DB"))
+.catch(() => console.log("Failed to connect to DB"))
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+  next();
+});
+
+app.use(express.json());
+
+app.get("/", function(req, res) {
+  res.send(console.log("First test"))
+})
+
+// app.use('/api/user', userRoutes)
+
+module.exports = app;
