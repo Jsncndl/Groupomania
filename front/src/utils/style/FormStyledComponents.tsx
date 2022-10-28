17c64bdb7c6811542cbe8a8ae75ebd906d41648b
Jsncndl <jsncndl1@gmail.com>
2022-10-28 15:24:01 +0200

    front(style): separate styled components form from component, add css breakpoints

---
+++ front/src/utils/style/FormStyledComponents.tsx
@@ -0,0 +1,59 @@
+import styled from "styled-components";
+import colors from "./colors";
+
+import { mediaQueries } from "./mediaQueries";
+
+export const FormContainer = styled.div`
+  width: 95%;
+  background: white;
+  border-radius: 15px;
+  box-shadow: 0 0 4px ${colors.tertiary};
+  margin: 25px auto;
+
+  @media (min-width: ${mediaQueries.large}) {
+    width: 60%;
+  }
+`
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  padding: 15px 3%;
+  margin: auto;
+`
+
+export const Label =styled.label`
+  padding: 0 10px 5px;
+`
+
+export const Input = styled.input((props) => ({
+  width: "80%",
+  margin: "0 0 10px",
+  padding: "5px",
+  border: props.color + " solid 2px"
+}))
+
+export const TextArea = styled.textarea((props) => ({
+  padding: "5px",
+  minWidth: "75%",
+  maxWidth: "90%",
+  minHeight: "40%",
+  maxHeight: "300px",
+  fontFamily: "Lato, sans-serif",
+  margin: "0 0 10px",
+  border: props.color + " solid 2px"
+}))
+
+export const UploadButton = styled.label`
+  text-align: center;
+  padding: 5px 10px;
+  border-radius: 20px;
+  background-color: ${colors.primary};
+  color: white;
+  width: fit-content;
+  cursor: pointer;
+`
+
+export const InputUploadFile = styled.input((props) => ({
+  display: "none"
+}))
---
+++ front/src/utils/style/mediaQueries.tsx
@@ -0,0 +1,9 @@
+const breakpoint_s = 576
+const breakpoint_m = 768
+const breakpoint_l = 1200
+
+export const mediaQueries = {
+  small: `${breakpoint_s}px`,
+  medium: `${breakpoint_m}px`,
+  large: `${breakpoint_l}px`
+}
